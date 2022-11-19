0bc7de370fb1ee46b613bdd53457e58e87e07096
Mohish Khadse <mohishkhadse3222@gmail.com>
2022-11-19 18:43:19 +0530

    2 commit

--- Controllers/adminController.js
+++ Controllers/adminController.js
@@ -3,4 +3,8 @@ const Site = require("../models/siteModel");
 exports.registerSite = async (req, res, next) => {
   console.log(req.body);
   const logs = await Site.create(req.body);
+  res.status(200).json({
+    status: "success",
+    message: "successfully added the site ",
+  });
 };
